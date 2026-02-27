4cb3b111e27fd94ee8b1de6c064cd56d6b69f5de
dvshapkin <dvshapkin@mail.ru>
2026-02-27 23:50:31 +0700

    fix

--- src/tree/file_tree.rs
+++ src/tree/file_tree.rs
@@ -1,41 +1,60 @@
 use std::path::{Component, Path};
 
-use crate::{List, SortedList};
-use super::node::Node;
+use super::node::{Childs, ComponentType, Node};
 
-pub struct FileTree<'a> {
-    root: Node<'a>
+pub struct FileTree {
+    root: Node,
 }
 
-impl<'a> FileTree<'a> {
+impl FileTree {
     pub fn new() -> Self {
-        Self { root: Node::new() }
+        Self {
+            root: Node::new("/", ComponentType::Dir),
+        }
     }
 
-    pub fn add(&mut self, path: &'a Path, is_file: bool) {
-        if path.is_relative() {
-            return; // TODO: Err
+    pub fn add_file(&mut self, path: &Path) -> Result<(), &'static str> {
+        if !path.is_absolute() {
+            return Err("Path must be absolute");
         }
-        if is_file {
-            if self.root.files.is_none() {
-                self.root.files = Some(SortedList::new());
-            }
-        } else {
-            if self.root.dirs.is_none() {
-                self.root.dirs = Some(SortedList::new());
-            }
+        if path.as_os_str() == "/" {
+            return Ok(());
         }
-        let files = self.root.files.as_mut().unwrap();
-        let dirs = self.root.dirs.as_mut().unwrap();
-        for component in path.components() {
-            if component == Component::RootDir {
-                continue
-            }
-            if is_file {
-                files.push(component);
-            } else {
-                dirs.push(component);
-            }
+
+        let components: Vec<_> = path.components().collect();
+
+        // Первый проход: создаём все необходимые директории
+        let last_node = self.ensure_dirs(&components)?;
+
+        // Второй проход: добавляем файл в последнюю директорию
+        let last_component = components.last().ok_or("Empty path")?;
+        let name = last_component.as_os_str();
+        let childs = last_node
+            .childs
+            .get_or_insert_with(|| Box::new(Childs::new()));
+
+        let node = Node::new(name.to_string_lossy().as_ref(), ComponentType::File);
+        childs.files.insert(name.to_owned(), node);
+
+        Ok(())
+    }
+
+    fn ensure_dirs(&mut self, components: &[Component<'_>]) -> Result<&mut Node, &'static str> {
+        let mut current = &mut self.root;
+
+        // Пропускаем RootDir и последний компонент (файл)
+        for component in components.iter().skip(1).take(components.len() - 2) {
+            let name = component.as_os_str();
+
+            let childs = current
+                .childs
+                .get_or_insert_with(|| Box::new(Childs::new()));
+            current = childs
+                .dirs
+                .entry(name.to_owned())
+                .or_insert_with(|| Node::new(name.to_string_lossy().as_ref(), ComponentType::Dir));
         }
+
+        Ok(current)
     }
 }
--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,32 +1,65 @@
 use std::cmp::Ordering;
-use std::path::Component;
+use std::collections::BTreeMap;
+use std::ffi::OsString;
 
-use crate::SortedList;
-
-pub struct Node<'a> {
-    pub name: Component<'a>,
-    pub files: Option<SortedList<Component<'a>>>,
-    pub dirs: Option<SortedList<Component<'a>>>,
+#[derive(Debug, PartialEq)]
+pub enum ComponentType {
+    Dir,
+    File,
 }
 
-impl<'a> Node<'a> {
+pub struct Childs {
+    pub dirs: Box<BTreeMap<OsString, Node>>,
+    pub files: Box<BTreeMap<OsString, Node>>,
+}
+
+impl Childs {
     pub fn new() -> Self {
         Self {
-            name: Component::RootDir,
-            files: None,
-            dirs: None,
+            dirs: Box::new(BTreeMap::new()),
+            files: Box::new(BTreeMap::new())
         }
     }
 }
 
-impl<'a> PartialEq for Node<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
+pub struct Node {
+    pub name: OsString,
+    pub r#type: ComponentType,
+    pub childs: Option<Box<Childs>>,
+}
+
+impl Node {
+    pub fn new(name: &str, r#type: ComponentType) -> Self {
+        Self {
+            name: OsString::from(name),
+            r#type,
+            childs: None,
+        }
     }
 }
 
-impl<'a> PartialOrd for Node<'a> {
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.r#type == other.r#type
+    }
+}
+
+impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.r#type != other.r#type {
+            return None;
+        }
         self.name.partial_cmp(&other.name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_node() {
+        println!("Node = {}", size_of::<Node>());
+        println!("Childs = {}", size_of::<Childs>());
     }
 }
